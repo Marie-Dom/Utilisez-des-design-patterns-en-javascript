ccd54c67c56f74d03d1b20159bae37f92e78ceb2
Marie-Dominique JAOUEN <md.friot@gmail.com>
2023-12-20 14:51:40 +0100

    making partie-2/chapitre-2-debut

--- js/App.js
+++ js/App.js
@@ -1,24 +1,31 @@
 class App {
-    constructor() {
-        this.$moviesWrapper = document.querySelector('.movies-wrapper')
-        this.moviesApi = new MovieApi('/data/new-movie-data.json')
-    }
+  constructor() {
+    this.$moviesWrapper = document.querySelector(".movies-wrapper");
+    this.oldMoviesApi = new MovieApi("/data/old-movie-data.json");
+    this.newMoviesApi = new MovieApi("/data/new-movie-data.json");
+  }
 
-    async main() {
-        // Ici je récupère mes films de mon fichier old-movie-data.json
-        const moviesData = await this.moviesApi.getMovies()
-        
-        moviesData
-            // Ici, je transforme mon tableau de données en un tableau de classe Movie
-            .map(movie => new Movie(movie))
-            .forEach(movie => {
-                const Template = new MovieCard(movie)
-                this.$moviesWrapper.appendChild(
-                    Template.createMovieCard()
-                )
-            })
-    }
+  async main() {
+    // Ici je récupère mes films de mon fichier old-movie-data.json
+    const oldMoviesData = await this.oldMoviesApi.getMovies();
+    const newMoviesData = await this.newMoviesApi.getMovies();
+
+    const OldMovies = oldMoviesData.map(
+      (movie) => new MoviesFactory(movie, "oldApi")
+    );
+    const NewMovies = newMoviesData.map(
+      (movie) => new MoviesFactory(movie, "newApi")
+    );
+
+    const FullMovies = OldMovies.concat(NewMovies);
+    // Ici, je transforme mon tableau de données en un tableau de classe Movie
+    // .map(movie => new Movie(movie))
+    FullMovies.forEach((movie) => {
+      const Template = new MovieCard(movie);
+      this.$moviesWrapper.appendChild(Template.createMovieCard());
+    });
+  }
 }
 
-const app = new App()
-app.main()
+const app = new App();
+app.main();
