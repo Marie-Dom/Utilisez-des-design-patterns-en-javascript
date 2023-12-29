98063ba5884ab151b57d755de9907d03e860bf69
Marie-Dominique JAOUEN <md.friot@gmail.com>
2023-12-29 15:47:35 +0100

    making quizz question-3

--- js/App.js
+++ js/App.js
@@ -1,17 +1,38 @@
-const fetchData = () => fetch('/data/data.json')
-    .then(res => res.json())
-    .catch(e => console.log('error', e))
-
+const fetchData = () =>
+  fetch("/data/data.json")
+    .then((res) => res.json())
+    .catch((e) => console.log("error", e));
 
 class Movie {
-}
+  constructor(data) {
+    this._id = data.id;
+    this._title = data.title;
+    this._img = data.img;
+    this._rating = data.rating;
+  }
 
+  get id() {
+    return this._id;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get img() {
+    return this._img;
+  }
+
+  get rating() {
+    return this._rating;
+  }
+}
 
 class App {
-    async init() {
-        const data = await fetchData()
-    }
+  async init() {
+    const data = await fetchData();
+  }
 }
 
-const app = new App()
-app.init()
+const app = new App();
+app.init();
