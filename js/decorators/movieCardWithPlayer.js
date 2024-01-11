fbb511754e9b34d95347c464eb2327c473aadaca
Marie-Dominique JAOUEN <md.friot@gmail.com>
2024-01-11 12:40:10 +0100

    Making course part-3 chapter-2

--- js/decorators/movieCardWithPlayer.js
+++ js/decorators/movieCardWithPlayer.js
@@ -1,2 +1,9 @@
 function movieCardWithPlayer(movieCard) {
+  if (movieCard.movie.actor === "sylvester") {
+    movieCard.$wrapper.addEventListener("click", () => {
+      const Player = new playerModal(movieCard.movie);
+      Player.render();
+    });
+  }
+  return movieCard;
 }
